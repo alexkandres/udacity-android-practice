9270ff240dc4a129acb2779fdbce41667585d23c
alexkandres <alexkandres@live.com>
2017-02-03 16:40:48 -0500

    Add inner class for contract definition

--- app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
+++ app/src/main/java/com/example/android/waitlist/data/WaitlistContract.java
@@ -7,11 +7,20 @@ public class WaitlistContract {
 
 
     // TODO (1) Create an inner class named WaitlistEntry class that implements the BaseColumns interface
-
+    public class WaitlistEntry implements BaseColumns{
         // TODO (2) Inside create a static final members for the table name and each of the db columns
         // TABLE_NAME -> waitlist;
         // COLUMN_GUEST_NAME -> guestName
         // COLUMN_PARTY_SIZE -> partySize
         // COLUMN_TIMESTAMP -> timestamp
+
+        //table name
+        static final String TABLE_NAME = "waitlist";
+
+        //column names guestName, partySize, timestamp
+        static final String COLUMN_GUEST_NAME = "guestName";
+        static final String COLUMN_PARTY_SIZE = "partySize";
+        static final String COLUMN_TIMESTAMP = "timestamp";
+    }
 
 }
